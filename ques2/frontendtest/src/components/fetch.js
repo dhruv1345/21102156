47f792f2bb3a038bf915b3d8abe62c792351b756
dhruv1345 <21102156@mail.jiit.ac.in>
2024-03-27 16:28:11 +0530

    started with react ques. But didn't get how to fetch from developed api.

---
+++ ques2/frontendtest/src/components/fetch.js
@@ -0,0 +1,16 @@
+// import React, {useState, useEffect} from "react";
+
+// function fetchComp(){  
+//     const [data, setData] = useState(null);
+
+//     useEffect(()=>{
+//         const fetchingData = async()=>{
+//             try{
+//                 const res = await fetch("http://localhost:8080/api/test/products");
+//             }
+//             catch(err){
+
+//             }
+//         }
+//     })
+// }
